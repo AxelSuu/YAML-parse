5ef41ecf80b188b9722bf32ec30ff984d4e7ba8b
Axel <axel@email.com>
2026-05-21 14:17:51 +0200

    Addded yaml parse time benchmarking

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include "../include/parser.h"
 
 int main(int argc, char* argv[]){
@@ -8,11 +9,17 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
+    // setting up time test
+    struct timespec t0, t1;
+
     YamlMap map = {0};
+    clock_gettime(CLOCK_MONOTONIC, &t0);
     if (!parse(argv[1], &map)) {
         fprintf(stderr, "Parse failed\n");
         return 1;
     }
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    printf("Parse time: %ld ns\n", (t1.tv_sec - t0.tv_sec) * 1000000000 + (t1.tv_nsec - t0.tv_nsec));
 
     for (int i = 0; i < map.count; i++)
         printf("%s = %s\n", map.entries[i].key, map.entries[i].value);
